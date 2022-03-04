aa2c54b0c9426f00b6c3f06523b284c7b0a88362
Ankit Negi <89020930+serAnkii@users.noreply.github.com>
2022-03-04 10:31:46 +0530

    Create index.js

---
+++ Full Stack Web Development/Practical/Lab5/index.js
@@ -0,0 +1,11 @@
+
+ function converttoinch() {
+    var val = document.getElementById("conv").value;
+    document.getElementById("some").innerHTML=val*12;
+}
+
+
+ function converttofoot() {
+    var val = document.getElementById("conv").value;
+    document.getElementById("some").innerHTML=val/12;
+}
